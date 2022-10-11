c6708c5a869a1d3fa82b01a1e03432c086db7375
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2022-10-11 22:44:22 +0530

    esp32 webserver host mode and station mode

--- nodemcu_led_blink/nodemcu_led_blink.ino
+++ nodemcu_led_blink/nodemcu_led_blink.ino
@@ -1,23 +1,10 @@
-#include<ESP8266WiFi.h>
-#include<Blynk.h>
-#include<BlynkSympleESP8266.h>
-char auth[]="fdlWcmQ2jchnpYqEakrwqYIOoxHim6";
-char ssid[]="tirtha's realme3";
-char pass[]="9876543210";
-BlynkTimer timer;
-void MS()
-{
-  float m=analogRead(A0);
-  Blynk.virtualWrite(V0,m);
-  
-}
+int LED = 12;
 void setup() {
- Serial.begin(9600);
- Blynk.begin(auth,ssid,pass);
- timer.setInterval(1000L,MS);
+pinMode (LED, OUTPUT);
 }
-
 void loop() {
- Blynk.run()
- timer.run()
+digitalWrite(LED, HIGH);
+delay(1000);
+digitalWrite(LED, LOW);
+delay(1000);
 }
