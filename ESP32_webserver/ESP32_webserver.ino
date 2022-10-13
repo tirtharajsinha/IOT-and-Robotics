aa088292c7972284be49d4d87c8ac4a2ae3ba5dd
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2022-10-14 04:09:30 +0530

    upgraded the esp webserver(ap & sta)

--- ESP32_webserver/ESP32_webserver.ino
+++ ESP32_webserver/ESP32_webserver.ino
@@ -1,5 +1,6 @@
 #include <WiFi.h>
-#include <WebServer.h>
+#include "index.h"
+#include <ESPAsyncWebServer.h>
 
 /* Put your SSID & Password */
 const char* ssid = "ESP32";         // Enter SSID here
@@ -10,35 +11,40 @@ IPAddress local_ip(192, 168, 1, 1);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
-WebServer server(80);
+AsyncWebServer server(80);
 
-uint8_t LED1pin = 4;
+uint8_t LED1pin = 2;
 bool LED1status = LOW;
 
 uint8_t LED2pin = 5;
 bool LED2status = LOW;
+
+Connact_AP(){
+   WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+}
 
 void setup() {
   Serial.begin(115200);
   pinMode(LED1pin, OUTPUT);
   pinMode(LED2pin, OUTPUT);
 
-  WiFi.softAP(ssid, password);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
-  delay(100);
+  Connact_AP();
+ 
 
   server.on("/", handle_OnConnect);
-  server.on("/led1on", handle_led1on);
-  server.on("/led1off", handle_led1off);
-  server.on("/led2on", handle_led2on);
-  server.on("/led2off", handle_led2off);
-  server.onNotFound(handle_NotFound);
+  server.on("/led1", HTTP_GET, handle_led1on);
+  server.on("/led2", HTTP_GET, handle_led2on);
+  server.onNotFound([](AsyncWebServerRequest* request) {
+    request->send(404);
+  });
 
   server.begin();
   Serial.println("HTTP server started");
 }
 void loop() {
-  server.handleClient();
+  // server.handleClient();
   if (LED1status) {
     digitalWrite(LED1pin, HIGH);
   } else {
@@ -52,72 +58,70 @@ void loop() {
   }
 }
 
-void handle_OnConnect() {
-  LED1status = LOW;
-  LED2status = LOW;
+void handle_OnConnect(AsyncWebServerRequest* request) {
   Serial.println("GPIO4 Status: OFF | GPIO5 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status, LED2status));
+  request->send_P(200, "text/html", index_html, processor);
 }
 
-void handle_led1on() {
-  LED1status = HIGH;
-  Serial.println("GPIO4 Status: ON");
-  server.send(200, "text/html", SendHTML(true, LED2status));
-}
+void handle_led1on(AsyncWebServerRequest* request) {
+  String ledreq = "";
+  if (request->hasParam("state")) {
+    ledreq = request->getParam("state")->value();
+  }
+  Serial.println("led request" + ledreq);
 
-void handle_led1off() {
-  LED1status = LOW;
-  Serial.println("GPIO4 Status: OFF");
-  server.send(200, "text/html", SendHTML(false, LED2status));
-}
-
-void handle_led2on() {
-  LED2status = HIGH;
-  Serial.println("GPIO5 Status: ON");
-  server.send(200, "text/html", SendHTML(LED1status, true));
-}
-
-void handle_led2off() {
-  LED2status = LOW;
-  Serial.println("GPIO5 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status, false));
-}
-
-void handle_NotFound() {
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(uint8_t led1stat, uint8_t led2stat) {
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>LED Control</title>\n";
-  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr += ".button-on {background-color: #3498db;}\n";
-  ptr += ".button-on:active {background-color: #2980b9;}\n";
-  ptr += ".button-off {background-color: #34495e;}\n";
-  ptr += ".button-off:active {background-color: #2c3e50;}\n";
-  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr += "</style>\n";
-  ptr += "</head>\n";
-  ptr += "<body>\n";
-  ptr += "<h1>ESP32 Web Server</h1>\n";
-  ptr += "<h3>Using Access Point(AP) Mode</h3>\n";
-
-  if (led1stat) {
-    ptr += "<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";
-  } else {
-    ptr += "<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";
+  if (ledreq == "ON") {
+    LED1status = HIGH;
+    Serial.println("GPIO4 Status: ON");
+  } else if (ledreq = "OFF") {
+    LED1status = LOW;
+    Serial.println("GPIO4 Status: OFF");
   }
 
-  if (led2stat) {
-    ptr += "<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";
-  } else {
-    ptr += "<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";
+  request->redirect("/");
+}
+
+
+
+void handle_led2on(AsyncWebServerRequest* request) {
+  String ledreq = "";
+  if (request->hasParam("state")) {
+    ledreq = request->getParam("state")->value();
   }
 
-  ptr += "</body>\n";
-  ptr += "</html>\n";
-  return ptr;
+  if (ledreq == "ON") {
+    LED2status = HIGH;
+    Serial.println("GPIO5 Status: ON");
+  } else if (ledreq = "OFF") {
+    LED2status = LOW;
+    Serial.println("GPIO5 Status: OFF");
+  }
+
+  request->redirect("/");
+}
+
+
+
+String processor(const String& var) {
+  //Serial.println(var);
+  if (var == "BUTTON_TYPE") {
+    String buttons = "";
+    
+    if (LED1status) {
+      buttons += btn1off;
+    } else {
+      buttons += btn1on;
+    }
+
+    if (LED2status) {
+      buttons += btn2off;
+    } else {
+      buttons += btn2on;
+    }
+
+
+
+    return buttons;
+  }
+  return String();
 }
--- ESP32_webserver_client_mode/ESP32_webserver_client_mode.ino
+++ ESP32_webserver_client_mode/ESP32_webserver_client_mode.ino
@@ -1,11 +1,12 @@
 #include <WiFi.h>
-#include <WebServer.h>
+#include <ESPAsyncWebServer.h>
+#include "index.h"
 
 /*Put your SSID & Password*/
 const char* ssid = "Tirtha";      // Enter SSID here
 const char* password = "12233344440";  //Enter Password here
 
-WebServer server(80);
+AsyncWebServer server(80);
 
 uint8_t LED1pin = 2;
 bool LED1status = LOW;
@@ -13,17 +14,13 @@ bool LED1status = LOW;
 uint8_t LED2pin = 5;
 bool LED2status = LOW;
 
-void setup() {
-  Serial.begin(115200);
+void Connect_STATION(){
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-  delay(100);
-  delay(100);
-  pinMode(LED1pin, OUTPUT);
-  pinMode(LED2pin, OUTPUT);
 
-  Serial.println("Connecting to ");
+  Serial.print("Connecting to : ");
   Serial.println(ssid);
+
 
   //connect to your local wi-fi network
   WiFi.begin(ssid, password);
@@ -39,26 +36,32 @@ void setup() {
   Serial.println(WiFi.localIP());
   WiFi.setAutoReconnect(true);
   WiFi.persistent(true);
+  delay(100);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Connect_STATION();
+
+  pinMode(LED1pin, OUTPUT);
+  pinMode(LED2pin, OUTPUT);
+
 
   server.on("/", handle_OnConnect);
-  server.on("/led1on", handle_led1on);
-  server.on("/led1off", handle_led1off);
-  server.on("/led2on", handle_led2on);
-  server.on("/led2off", handle_led2off);
-  server.onNotFound(handle_NotFound);
+  server.on("/led1", HTTP_GET, handle_led1on);
+  // server.on("/led1", handle_led1off);
+  server.on("/led2", HTTP_GET, handle_led2on);
+
+  server.onNotFound([](AsyncWebServerRequest* request) {
+    request->send(404);
+  });
 
   server.begin();
   Serial.println("HTTP server started");
 }
 void loop() {
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print("Reconnecting");
-    WiFi.disconnect();
-    WiFi.begin(ssid, password);
-  }
-  server.handleClient();
-
-
+  // server.handleClient();
   if (LED1status) {
     digitalWrite(LED1pin, HIGH);
   } else {
@@ -72,78 +75,70 @@ void loop() {
   }
 }
 
-void handle_OnConnect() {
+void handle_OnConnect(AsyncWebServerRequest* request) {
   Serial.println("GPIO4 Status: OFF | GPIO5 Status: OFF");
-  server.send(200, "text/html", SendHTML(LED1status, LED2status));
+  request->send_P(200, "text/html", index_html, processor);
 }
 
-void handle_led1on() {
-  LED1status = HIGH;
-  Serial.println("GPIO4 Status: ON");
-  // server.send(200, "text/html", SendHTML(true,LED2status));
-  server.sendHeader("Location", "/", true);
-  server.send(302, "text/plain", "");
-}
+void handle_led1on(AsyncWebServerRequest* request) {
+  String ledreq = "";
+  if (request->hasParam("state")) {
+    ledreq = request->getParam("state")->value();
+  }
+  Serial.println("led request" + ledreq);
 
-void handle_led1off() {
-  LED1status = LOW;
-  Serial.println("GPIO4 Status: OFF");
-  // server.send(200, "text/html", SendHTML(false,LED2status));
-  server.sendHeader("Location", "/", true);
-  server.send(302, "text/plain", "");
-}
-
-void handle_led2on() {
-  LED2status = HIGH;
-  Serial.println("GPIO5 Status: ON");
-  // server.send(200, "text/html", SendHTML(LED1status,true));
-  server.sendHeader("Location", "/", true);
-  server.send(302, "text/plain", "");
-}
-
-void handle_led2off() {
-  LED2status = LOW;
-  Serial.println("GPIO5 Status: OFF");
-  // server.send(200, "text/html", SendHTML(LED1status,false));
-  server.sendHeader("Location", "/", true);
-  server.send(302, "text/plain", "");
-}
-
-void handle_NotFound() {
-  server.send(404, "text/plain", "Not found");
-}
-
-String SendHTML(uint8_t led1stat, uint8_t led2stat) {
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>LED Control</title>\n";
-  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr += ".button {display: block;width: 80px;background-color: #3498db;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr += ".button-on {background-color: #3498db;}\n";
-  ptr += ".button-on:active {background-color: #2980b9;}\n";
-  ptr += ".button-off {background-color: #34495e;}\n";
-  ptr += ".button-off:active {background-color: #2c3e50;}\n";
-  ptr += "p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr += "</style>\n";
-  ptr += "</head>\n";
-  ptr += "<body>\n";
-  ptr += "<h1>ESP32 Web Server</h1>\n";
-  ptr += "<h3>Using Station(STA) Mode</h3>\n";
-
-  if (led1stat) {
-    ptr += "<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";
-  } else {
-    ptr += "<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";
+  if (ledreq == "ON") {
+    LED1status = HIGH;
+    Serial.println("GPIO4 Status: ON");
+  } else if (ledreq = "OFF") {
+    LED1status = LOW;
+    Serial.println("GPIO4 Status: OFF");
   }
 
-  if (led2stat) {
-    ptr += "<p>LED2 Status: ON</p><a class=\"button button-off\" href=\"/led2off\">OFF</a>\n";
-  } else {
-    ptr += "<p>LED2 Status: OFF</p><a class=\"button button-on\" href=\"/led2on\">ON</a>\n";
+  request->redirect("/");
+}
+
+
+
+void handle_led2on(AsyncWebServerRequest* request) {
+  String ledreq = "";
+  if (request->hasParam("state")) {
+    ledreq = request->getParam("state")->value();
   }
 
-  ptr += "</body>\n";
-  ptr += "</html>\n";
-  return ptr;
+  if (ledreq == "ON") {
+    LED2status = HIGH;
+    Serial.println("GPIO5 Status: ON");
+  } else if (ledreq = "OFF") {
+    LED2status = LOW;
+    Serial.println("GPIO5 Status: OFF");
+  }
+
+  request->redirect("/");
+}
+
+
+
+String processor(const String& var) {
+  //Serial.println(var);
+  if (var == "BUTTON_TYPE") {
+    String buttons = "";
+    
+    if (LED1status) {
+      buttons += btn1off;
+    } else {
+      buttons += btn1on;
+    }
+
+    if (LED2status) {
+      buttons += btn2off;
+    } else {
+      buttons += btn2on;
+    }
+
+
+
+    return buttons;
+  }
+  return String();
 }
