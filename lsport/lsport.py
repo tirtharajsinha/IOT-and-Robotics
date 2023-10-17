41385ccbd631606871087e761dcb2bd2d9204e22
Tirtharaj Sinha <sinhatirtharaj@gmail.com>
2023-10-18 01:42:57 +0530

    Update lsport.py

--- lsport/lsport.py
+++ lsport/lsport.py
@@ -1,12 +1,21 @@
-import os
+import subprocess as sp
+
 from tabulate import tabulate
 
-res = os.popen("ls -l /dev/serial/by-id/ | awk '{print $9 \" \" $11}'",'r',1)
+pipe = sp.Popen("ls -l /dev/serial/by-id/ | awk '{print $9 \" \" $11}'",shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+res = pipe.communicate()
 
-ports=res.read().strip()
+
+ports=res[0].decode(encoding='utf-8').strip()
 
 data= []
-ports=ports.split("\n")
+
+if ports=="":
+	ports=[]
+else:
+	ports=ports.split("\n")
+
+
 for port in ports:
 	port=port.split()
 	data.append([port[0],port[1].replace("../..","/dev")])
@@ -15,4 +24,7 @@ print()
 print (tabulate(data, headers=["Device Name", "Port"]))
 print()
 print(f"{len(ports)} Devices Found\n")
+
+print("If you don't see your device here, Then run lsusb -t to find the driver is install or not.")
+
 
